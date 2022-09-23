f9734cf5308ecd5d86958f4c5ae6e1edf064276d
Manish Shivabhakti <manish@learnr.app>
2022-09-23 12:04:09 +0545

    Add auth routes

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './db.js';
+import cors from 'cors';
+
+// import routes
+import authRoutes from './routes/auth/auth.js';
 
 dotenv.config();
 
@@ -14,12 +18,20 @@ const NODE_ENV = process.env.NODE_ENV || 'development';
 // connect database (development or production)
 connectDB(NODE_ENV);
 
+// middlewares
+app.use(express.json());
+app.use(cors());
+
 // dev logging middleware
 if (NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.get('/', (req, res) => {
+// routes
+
+app.use('/api/v1/auth', authRoutes);
+
+app.get('/api/v1', (req, res) => {
   res.json({ message: 'Hello World' });
 });
 
