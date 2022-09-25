4d01a8620570dafa977810410ac9e2a29f7feea9
Manish Shivabhakti <manish@learnr.app>
2022-09-25 18:03:48 +0545

    Update async function with HOF

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,48 +1,40 @@
 import User from '../models/User.js';
+import AppError from '../utils/AppError.js';
+import catchAsync from '../utils/catchAsync.js';
 
-export const signup = async (req, res, next) => {
+export const signup = catchAsync(async (req, res, next) => {
   const { firstname, lastname, username, email, password } = req.body;
 
-  try {
-    // check if user with email already exits already exists
-    const emailExists = await User.findOne({ email });
-    if (emailExists) {
-      return res.status(400).json({
-        message: 'Email is taken. Please login.',
-      });
-    }
-
-    // check if user with username already exits already exists
-    const usernameExists = await User.findOne({ username });
-    if (usernameExists) {
-      return res.status(400).json({
-        message: 'Username is taken.',
-      });
-    }
-
-    // create new user
-    const user = new User({
-      firstname,
-      lastname,
-      username,
-      email,
-      password,
-    });
-    await user.save();
-    res.status(201).json({
-      message: 'User created.',
-      data: {
-        user,
-      },
-    });
-  } catch (err) {
-    return res.status(400).json({
-      message: 'Something went wrong.',
-    });
+  // check if user with email already exits already exists
+  const emailExists = await User.findOne({ email });
+  if (emailExists) {
+    return next(new AppError('Email already exists.', 400));
   }
-};
+  // check if user with username already exits already exists
+  const usernameExists = await User.findOne({ username });
+  if (usernameExists) {
+    return next(new AppError('Username is taken.', 400));
+  }
 
-export const login = async (req, res, next) => {
+  // create new user
+  const user = new User({
+    firstname,
+    lastname,
+    username,
+    email,
+    password,
+  });
+  await user.save();
+  res.status(201).json({
+    status: 'success',
+    message: 'User created.',
+    data: {
+      user,
+    },
+  });
+});
+
+export const login = catchAsync(async (req, res, next) => {
   // extract the req body
   const { email, password } = req.body;
 
@@ -51,24 +43,21 @@ export const login = async (req, res, next) => {
 
   // if user does not exist
   if (!user) {
-    return res.status(400).json({
-      message: "User doesn't exist. Please signup.",
-    });
+    return next(new AppError(`User doesn't exist.`, 400));
   } else {
     // if user exists
     // check if the password is correct
     const isCorrectPassword = await user.isCorrectPassword(password);
     if (isCorrectPassword) {
       res.status(200).json({
+        status: 'success',
         message: 'Logged in successfully.',
         data: {
           user,
         },
       });
     } else {
-      res.status(200).json({
-        message: 'Incorrect email or password.',
-      });
+      return next(new AppError('Invalid email or password', 401));
     }
   }
-};
+});
