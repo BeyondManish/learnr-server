88dc2b57c2d0d4f4079cf7c84417fa49d3864b2f
Manish Shivabhakti <manish@learnr.app>
2022-10-14 08:47:10 +0545

    Remove console.log

--- validators/validationResult.js
+++ validators/validationResult.js
@@ -3,7 +3,6 @@ import { validationResult } from 'express-validator';
 const validator = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    console.log(errors);
     return res
       .status(422)
       .json({ errors: errors.array().map((error) => error.msg) });
