d3f7dccbe826010fb46b1cc9a237c87c0a7cd963
Manish Shivabhakti <manish@learnr.app>
2022-10-16 11:49:39 +0545

    Add tag model

---
+++ models/Tags.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const tagSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
+const Tag = mongoose.model("Tag", tagSchema);
+
+export default Tag;
