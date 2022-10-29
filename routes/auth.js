5c89a109286bc1a2f98558df4eb206ff07933e25
Manish Shivabhakti <manish@learnr.app>
2022-10-29 21:49:36 +0545

    Rename validation result

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,11 @@ import { Router } from 'express';
 import { login, signup } from '../controllers/auth.js';
 import userSignUpValidator from '../validators/userSignUp.js';
 import userLogInValidator from '../validators/userLogIn.js';
-import validationResult from '../validators/validationResult.js';
+import isValid from '../validators/validationResult.js';
 
 const router = Router();
 
-router.post('/signup', userSignUpValidator, validationResult, signup);
-router.post('/login', userLogInValidator, validationResult, login);
+router.post('/signup', userSignUpValidator, isValid, signup);
+router.post('/login', userLogInValidator, isValid, login);
 
 export default router;
