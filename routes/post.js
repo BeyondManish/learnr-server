832150618b06a77a3c97b0713fe15ec9f6c8fced
Manish Shivabhakti <manish@learnr.app>
2022-11-14 20:49:21 +0545

    Get single post api

--- routes/post.js
+++ routes/post.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import postValidator from '../validators/postValidator.js';
 import { requireLogin, isAdmin } from "../controllers/auth.js";
-import { create, getAllPosts } from '../controllers/post.js';
+import { create, read, getAllPosts } from '../controllers/post.js';
 import isValid from '../validators/validationResult.js';
 
 const router = Router();
 
 router.get("/", getAllPosts);
 router.post("/create-post", requireLogin, isAdmin, postValidator, isValid, create);
+router.get("/:author/:slug", read);
 
 export default router;
