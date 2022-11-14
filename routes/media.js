14a8c39d8d9d847c93d889e1ce341d13b23195a5
Manish Shivabhakti <manish@learnr.app>
2022-11-14 18:26:31 +0545

    Add media routes (get files and delete)

--- routes/media.js
+++ routes/media.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { uploadImage } from "../controllers/media.js";
+import { uploadImage, loadAllMedias, deleteImage } from "../controllers/media.js";
 import { requireLogin, isAdmin } from "../controllers/auth.js";
 
 const router = Router();
 
+router.get("/files", requireLogin, isAdmin, loadAllMedias);
 router.post("/files/upload-image", requireLogin, isAdmin, uploadImage);
+router.delete("/files/delete/:id", requireLogin, isAdmin, deleteImage);
 
 export default router;
