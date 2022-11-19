5acbaca6e8250155886ac6bf106ba2c65645e62d
Manish Shivabhakti <manish@learnr.app>
2022-11-19 23:24:27 +0545

    Add user functions

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 import catchAsync from "../utils/catchAsync.js";
 import User from "../models/User.js";
+import AppError from "../utils/AppError.js";
 
 export const getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -14,6 +15,53 @@ export const getAllUsers = catchAsync(async (req, res, next) => {
     message: "All users fetched",
     data: {
       users,
+    }
+  });
+});
+
+export const getUser = catchAsync(async (req, res, next) => {
+  const { username } = req.params;
+  const user = await User.findOne({ username });
+  console.log(user);
+  if (!user) {
+    return next(new AppError("User not found", 404));
+  }
+  res.status(200).json({
+    status: "success",
+    message: "User fetched successfully",
+    data: {
+      user,
+    }
+  });
+});
+
+export const deleteUser = catchAsync(async (req, res, next) => {
+  const { username } = req.params;
+  const user = await User.findOne({ username });
+  if (!user) {
+    return next(new AppError("User not found", 404));
+  }
+  await user.remove();
+  res.status(200).json({
+    status: "success",
+    message: "User deleted successfully",
+  });
+});
+
+export const editUser = catchAsync(async (req, res, next) => {
+  const { username } = req.params;
+  const { firstname, lastname, email, photo } = req.body;
+  const user = await User.findOneAndUpdate({ username }, {
+    firstname, lastname, email, photo
+  });
+  if (!user) {
+    return next(new AppError("User not found", 404));
+  }
+  res.status(200).json({
+    status: "success",
+    message: "User updated successfully",
+    data: {
+      user,
     }
   });
 });
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { isAdmin, requireLogin } from '../controllers/auth.js';
-import { getAllUsers } from "../controllers/user.js";
+import { getAllUsers, getUser } from "../controllers/user.js";
 
 const router = Router();
 
 router.get('/users', requireLogin, isAdmin, getAllUsers);
+router.get('/user/:username', getUser);
 
 export default router;
