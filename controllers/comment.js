9f0908a38b332158db8f9bb3b6817cd68260f013
Manish Shivabhakti <manish@learnr.app>
2022-11-22 23:28:23 +0545

    Populate before sending comment

--- controllers/comment.js
+++ controllers/comment.js
@@ -5,11 +5,11 @@ export const create = catchAsync(async (req, res, next) => {
   // get post id from the slug in url and comment author from the req.url
   const userId = req.user._id;
   const { comment, postId } = req.body;
-  console.log(comment, postId);
   const newComment = await Comment.create({ author: userId, comment: comment, post: postId });
-  const commentDetail = await (await newComment.populate("author", "firstname lastname username photo")).populate("post", "title slug");
+  console.log(newComment);
+  const commentDetail = await Comment.findById(newComment._id).populate("author", "firstname lastname username photo").populate("post", "title slug");
+  console.log(commentDetail);
   res.status(201).json({ status: "success", message: "Comment posted successfully", comment: commentDetail });
-  //
 });
 
 export const getAllComments = catchAsync(async (req, res, next) => {
@@ -36,12 +36,11 @@ export const remove = catchAsync(async (req, res, next) => {
 
 export const getPostComments = catchAsync(async (req, res, next) => {
   const postId = req.params.postId;
-  console.log(postId);
-  const comments = await Comment.find({ post: postId });
-  console.log(comments);
+  const comments = await Comment.find({ post: postId })
+    .populate("author", "firstname lastname username photo");
   return res.status(200).json({
     status: "success",
     message: "All comment fetched successfully",
-    data: comments
+    data: { comments }
   });
 });
